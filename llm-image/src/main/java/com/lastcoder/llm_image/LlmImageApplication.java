90a54aa0125ba006ea50576f8600c4066569803d
lastcoder <jonghyeok.bae@gmail.com>
2026-05-13 16:24:37 +0900

    llm-image

---
+++ llm-image/src/main/java/com/lastcoder/llm_image/LlmImageApplication.java
@@ -0,0 +1,13 @@
+package com.lastcoder.llm_image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmImageApplication.class, args);
+	}
+
+}
---
+++ llm-image/src/test/java/com/lastcoder/llm_image/LlmImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastcoder.llm_image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
